d1ae99cb765762d8968759a0364dd2ae735984da
Macnair Rios Calixto <78324420+Macnair-Rios-Calixto@users.noreply.github.com>
2021-02-02 23:27:32 +0300

    Add files via upload

---
+++ Install-PowerShell.ps1
@@ -0,0 +1,27 @@
+#Install Current User
+
+if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
+    Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
+      'Az modules installed at the same time is not supported.')
+} else {
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser
+}
+
+
+
+
+#Install All Users
+
+if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
+    Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
+      'Az modules installed at the same time is not supported.')
+} else {
+    Install-Module -Name Az -AllowClobber -Scope AllUsers
+}
+
+
+
+
+#Connect to Azure with a browser sign in token
+
+Connect-AzAccount
