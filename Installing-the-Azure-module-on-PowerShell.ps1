72561a74015793853aca9938a8aef1f357d9d9eb
Macnair Rios Calixto <78324420+Macnair-Rios-Calixto@users.noreply.github.com>
2021-02-02 20:46:05 -0300

    Update Installing-the-Azure-module-on-PowerShell.ps1

--- Installing-the-Azure-module-on-PowerShell.ps1
+++ Installing-the-Azure-module-on-PowerShell.ps1
@@ -1,5 +1,4 @@
 #Install Current User
-
 if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
     Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
       'Az modules installed at the same time is not supported.')
@@ -7,11 +6,7 @@ if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -List
     Install-Module -Name Az -AllowClobber -Scope CurrentUser
 }
 
-
-
-
 #Install All Users
-
 if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
     Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
       'Az modules installed at the same time is not supported.')
@@ -20,8 +15,5 @@ if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -List
 }
 
 
-
-
 #Connect to Azure with a browser sign in token
-
 Connect-AzAccount
